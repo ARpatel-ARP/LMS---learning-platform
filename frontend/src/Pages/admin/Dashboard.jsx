6c15cc78b1ec48fc8da7fa3e5ebd1df1dc176840
ARpatel-ARP <rautanky407@gmail.com>
2026-04-22 11:59:04 +0530

    add CardContent to Dashboard component for total sales description

--- frontend/src/Pages/admin/Dashboard.jsx
+++ frontend/src/Pages/admin/Dashboard.jsx
@@ -1,4 +1,4 @@
-import { Card, CardHeader } from '@/components/ui/card'
+import { Card, CardContent, CardHeader } from '@/components/ui/card'
 import React from 'react'
 
 const Dashboard = () => {
@@ -6,6 +6,7 @@ const Dashboard = () => {
     <div className='grid gap-6 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4'>
       <Card>
         <CardHeader>Total Sales</CardHeader>
+        <CardContent>Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique, magni porro repellendus aut odio enim debitis mollitia dolorum nulla obcaecati minus explicabo autem vitae nihil pariatur? Voluptate deserunt debitis fugiat odio laudantium ipsum similique reprehenderit blanditiis ducimus, distinctio facere ullam, neque voluptatem quaerat veniam maxime.</CardContent>
       </Card>
     </div>
   )
