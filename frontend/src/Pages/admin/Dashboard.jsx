3a6a5fe097699134f614b98c0ae29d90be887277
ARpatel-ARP <rautanky407@gmail.com>
2026-04-23 09:40:34 +0530

    refactor Dashboard and Sidebar layout for improved structure and styling

--- frontend/src/Pages/admin/Dashboard.jsx
+++ frontend/src/Pages/admin/Dashboard.jsx
@@ -3,11 +3,14 @@ import React from 'react'
 
 const Dashboard = () => {
   return (
-    <div className='grid gap-6 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4'>
-      <Card>
-        <CardHeader>Total Sales</CardHeader>
-        <CardContent>Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique, magni porro repellendus aut odio enim debitis mollitia dolorum nulla obcaecati minus explicabo autem vitae nihil pariatur? Voluptate deserunt debitis fugiat odio laudantium ipsum similique reprehenderit blanditiis ducimus, distinctio facere ullam, neque voluptatem quaerat veniam maxime.</CardContent>
-      </Card>
+    <div className='p-10'>
+      <div className='grid gap-6 grid-cols-1 sm:grid-cols-2 lg:grid-cols- mt-10'>
+        <Card className="w-25">
+          <CardHeader className="">Total Sales</CardHeader>
+          <CardContent>$12,400</CardContent>
+        </Card>
+        {/* Add more stat cards here */}
+      </div>
     </div>
   )
 }
