50f8ffdc97fea58608872b98bb7783bdf6dbe0f8
ARpatel-ARP <rautanky407@gmail.com>
2026-04-25 08:58:37 +0530

    enhance AddCourse and CourseTable components with improved layout and new icon for course creation button

--- frontend/src/Pages/admin/course/AddCourse.jsx
+++ frontend/src/Pages/admin/course/AddCourse.jsx
@@ -1,13 +1,21 @@
-import React from 'react'
+import React from "react";
 
 const AddCourse = () => {
   return (
-    <div className='flex-1 mx-10' >
-        <div className='mb-4'>
-            <h1 className='font-bold text-xl'>Lets add Course</h1>
-        </div>
+    <div className="flex-1 mx-10 ">
+      <div className="mb-4">
+        <h1 className="font-bold text-xl">Lets add Course</h1>
+        <p className="text-sm py-3">
+          Adding a new course to the platform is a straightforward process that
+          allows administrators to expand the learning catalog and provide
+          students with fresh, engaging content. To get started, simply navigate
+          to the course creation section, where you will be prompted to fill in
+          essential details such as the course title, description, category, and
+          difficulty level.
+        </p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default AddCourse
+export default AddCourse;
