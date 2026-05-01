7ef43fe2cfbf114d6d7e6db6b7455360dab5751b
ARpatel-ARP <rautanky407@gmail.com>
2026-05-01 10:04:42 +0530

    fix: correct course retrieval logic in getCreatorCourse and improve error handling

--- backend/controllers/courseController.js
+++ backend/controllers/courseController.js
@@ -27,8 +27,20 @@ export const createCourse = async (req, res) => {
 export const getCreatorCourse = async (req, res) => {
     try {
         const userId = req.id
-        const courses = await Courses.find({creator})
+        const courses = await Courses.find({creator:userId})
+        if (!courses) {
+            return res.status(404).json({
+                course:[],
+                message:"Course not found"
+            })
+            return res.status(200).json({
+                courses,
+            })
+        }
     } catch (error) {
-        
+        console.log(error);
+        return res.status(500).json({
+            message:"Failed to create course"
+        })
     }
 }
