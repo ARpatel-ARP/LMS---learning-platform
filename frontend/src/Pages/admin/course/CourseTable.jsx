3572fb2a3140a22a73600a5526db93c76e6d7f92
ARpatel-ARP <rautanky407@gmail.com>
2026-05-04 19:17:45 +0530

    feat: add EditCourse component and update routing in App.jsx

--- frontend/src/Pages/admin/course/CourseTable.jsx
+++ frontend/src/Pages/admin/course/CourseTable.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from "react";
 import {
   Table,
   TableBody,
@@ -8,11 +8,11 @@ import {
   TableHead,
   TableHeader,
   TableRow,
-} from "@/components/ui/table"
-import { Button } from '@/components/ui/button'
-import { useNavigate } from 'react-router-dom'
-import { Badge, Edit, Edit2, PlusCircle } from 'lucide-react'
-import { useGetCreatorCourseQuery } from '@/features/api/courseApi'
+} from "@/components/ui/table";
+import { Button } from "@/components/ui/button";
+import { useNavigate } from "react-router-dom";
+import { Badge, Edit, Edit2, PlusCircle } from "lucide-react";
+import { useGetCreatorCourseQuery } from "@/features/api/courseApi";
 
 const invoices = [
   {
@@ -57,42 +57,55 @@ const invoices = [
     totalAmount: "$300.00",
     paymentMethod: "Credit Card",
   },
-]
+];
 
 const CourseTable = () => {
-  const {data, isLoading} = useGetCreatorCourseQuery()
-  const navigate = useNavigate()
-  if (isLoading) return <h1>Loading...</h1>
-  const courses = data?.courses || []
+  const { data, isLoading } = useGetCreatorCourseQuery();
+  const navigate = useNavigate();
+  if (isLoading) return <h1>Loading...</h1>;
+  const courses = data?.courses || [];
   console.log("data ->", data);
-  return <>
-    <div>
-      <Button onClick={() => navigate(`create`)} className="mb-10 mt-5">Create New Course
-        <PlusCircle size={22} />
-      </Button>
-    </div>
-    <Table>
-      <TableCaption>A list of your recent Courses</TableCaption>
-      <TableHeader>
-        <TableRow>
-          <TableHead className="w-25">Price</TableHead>
-          <TableHead>Status</TableHead>
-          <TableHead>Title</TableHead>
-          <TableHead className="text-right">Action</TableHead>
-        </TableRow>
-      </TableHeader>
-      <TableBody>
-        {courses.map((course) => (
-          <TableRow key={course._id}>
-            <TableCell className="font-medium">{course?.coursePrice || "NA"}</TableCell>
-            <TableCell><Button>{course.isPublished ? "Published" : "Draft"}</Button></TableCell>
-            <TableCell>{course.courseTitle}</TableCell>
-            <TableCell className="text-right"><Button variant='ghost'><Edit2/></Button></TableCell>
+  return (
+    <>
+      <div>
+        <Button onClick={() => navigate(`create`)} className="mb-10 mt-5">
+          Create New Course
+          <PlusCircle size={22} />
+        </Button>
+      </div>
+      <Table>
+        <TableCaption>A list of your recent Courses</TableCaption>
+        <TableHeader>
+          <TableRow>
+            <TableHead className="w-25">Price</TableHead>
+            <TableHead>Status</TableHead>
+            <TableHead>Title</TableHead>
+            <TableHead className="text-right">Action</TableHead>
           </TableRow>
-        ))}
-      </TableBody>
-    </Table>
-  </>
-}
+        </TableHeader>
+        <TableBody>
+          {courses.map((course) => (
+            <TableRow key={course._id}>
+              <TableCell className="font-medium">
+                {course?.coursePrice || "NA"}
+              </TableCell>
+              <TableCell>
+                <Button>{course.isPublished ? "Published" : "Draft"}</Button>
+              </TableCell>
+              <TableCell>{course.courseTitle}</TableCell>
+              <TableCell className="text-right">
+                <Button
+                  variant="ghost"
+                >
+                  <Edit2 />
+                </Button>
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </>
+  );
+};
 
-export default CourseTable
+export default CourseTable;
