61fddcc66f1a5761fc90545a03587476a4b65fc6
ARpatel-ARP <rautanky407@gmail.com>
2026-05-04 19:33:35 +0530

    fix: update EditCourse component layout and add link to lectures page

--- frontend/src/Pages/admin/course/EditCourse.jsx
+++ frontend/src/Pages/admin/course/EditCourse.jsx
@@ -1,9 +1,16 @@
+import { Button } from '@/components/ui/button';
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const EditCourse = () => {
   return (
-    <div>
-      
+    <div className='flex-1'>
+    <div className='flex items-center justify-between mb-5'>
+      <h1 className='font-bold text-xl'>Add information regarding Course</h1>
+      <Link>
+      <Button variant='link' className="font-medium text-large">Go to lectures pages</Button>
+      </Link>
+    </div>
     </div>
   );
 }
