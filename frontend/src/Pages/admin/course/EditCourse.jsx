844a047d87dc3e1267e4e14ac3f0fc49cce3b08d
ARpatel-ARP <rautanky407@gmail.com>
2026-05-05 08:23:31 +0530

    fix: add missing 'to' prop in Link component in EditCourse

--- frontend/src/Pages/admin/course/EditCourse.jsx
+++ frontend/src/Pages/admin/course/EditCourse.jsx
@@ -7,7 +7,7 @@ const EditCourse = () => {
     <div className='flex-1'>
     <div className='flex items-center justify-between mb-5'>
       <h1 className='font-bold text-xl flex justify-end'><span>Add information regarding Course</span>
-      <Link>
+      <Link to='lecture'>
       <Button variant="ghost" className="hover:text-blue-400">Go to lectures pages</Button>
       </Link>
       </h1>
