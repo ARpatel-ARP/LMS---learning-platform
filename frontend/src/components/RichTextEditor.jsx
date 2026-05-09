dd66f815fc88c14d59cee4a395682a98152d0dbf
ARpatel-ARP <rautanky407@gmail.com>
2026-05-09 08:31:39 +0530

    feat: add handleChange function to update input description in RichTextEditor

--- frontend/src/components/RichTextEditor.jsx
+++ frontend/src/components/RichTextEditor.jsx
@@ -2,6 +2,10 @@ import { useQuill } from 'react-quilljs';
 import 'quill/dist/quill.snow.css';
 
 const RichTextEditor = ({input, setInput}) => {
+  const handleChange = (content) => {
+    setInput({...input, description:content})
+  }
+  
   const { quill, quillRef } = useQuill();
   return <div ref={quillRef} />;
 };
