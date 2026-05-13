378492e0ad3c34b63f6a88d2de69cb8f59f86d1c
ARpatel-ARP <rautanky407@gmail.com>
2026-05-13 08:32:48 +0530

    feat: remove course price input field from CourseTab component

--- frontend/src/Pages/admin/course/CourseTab.jsx
+++ frontend/src/Pages/admin/course/CourseTab.jsx
@@ -120,17 +120,6 @@ const CourseTab = () => {
                 className="w-full"
                 />
               </div>
-              <div>
-                <Label className="my-2 mx-1">Price in (INR)</Label>
-                <Input
-                type="number"
-                name="coursePrice"
-                value={input.coursePrice}
-                onChange={changeEvenHandler}
-                placeholder="enter price"
-                className="w-full"
-                />
-              </div>
             </div>
               <div>
                 <Label className="my-2 mx-1">Course Thumbnail</Label>
