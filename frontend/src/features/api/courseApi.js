15ff845b0fc28be049bbc35a43df2752c948d9db
ARpatel-ARP <rautanky407@gmail.com>
2026-05-14 19:05:41 +0530

    feat: add duplicate Search button in HeroSection component

--- frontend/src/features/api/courseApi.js
+++ frontend/src/features/api/courseApi.js
@@ -2,7 +2,6 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import reducer from "../authSlice";
 
 const COURSE_API = "http://localhost:8000/api/v1/course"
-const COURSE_API = "http://localhost:8000/api/v1/course"
 
 export const courseApi = createApi({
     reducerPath:"courseApi",
