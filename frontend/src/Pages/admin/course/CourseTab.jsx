3f1ac60c9ee1039360c10399617a2740a5e8d374
ARpatel-ARP <rautanky407@gmail.com>
2026-05-22 07:38:34 +0530

    CardDescription for clarity in CourseTab component

--- frontend/src/Pages/admin/course/CourseTab.jsx
+++ frontend/src/Pages/admin/course/CourseTab.jsx
@@ -61,6 +61,9 @@ const CourseTab = () => {
             <CardDescription>
               Make changes to your courses here. Click save when you are done
             </CardDescription>
+            <CardDescription>
+              Make changes to your courses here. Click save when you are done
+            </CardDescription>
           </div>
           <div className="flex gap-2">
             <Button variant="outline">
