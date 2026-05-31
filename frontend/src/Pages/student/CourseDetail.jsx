c616a2789f76d7d3814cd19478e684a0a5b54e55
ARpatel-ARP <rautanky407@gmail.com>
2026-05-31 10:29:11 +0530

    created video card

--- frontend/src/Pages/student/CourseDetail.jsx
+++ frontend/src/Pages/student/CourseDetail.jsx
@@ -1,4 +1,11 @@
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Separator } from "@/components/ui/separator";
 import { BadgeInfo, Lock, PlayCircle } from "lucide-react";
 import React from "react";
 
@@ -12,17 +19,22 @@ const CourseDetail = () => {
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate
             rem maiores ipsam?
           </p>
-          <span>Created By : <span className="italic underline cursor-pointer">RAUT-stack</span> </span>
+          <span>
+            Created By :{" "}
+            <span className="italic underline cursor-pointer">
+              RAUT-stack
+            </span>{" "}
+          </span>
           <div className="flex items-center gap-2 text-sm">
-            <BadgeInfo size={16}/>
+            <BadgeInfo size={16} />
             <p className="italic font-light">Last updated 2026-3-14</p>
           </div>
           <p>Students enrolled: 1</p>
         </div>
       </div>
 
-      <div className="flex flex-col lg:flex-row justify-between max-w-7xl">
-        <div className=" m-5 lg:mx-15 lg:px-10 w-[85%] lg:w-1/2 ">
+      <div className="flex flex-col  lg:flex-row justify-between  max-w-[95%]">
+        <div className=" md:m-5 mx-auto lg:mx-15 lg:px-10 w-[85%] lg:w-4/5 md:text-left text-center">
           <h1 className="text-2xl font-bold mb-3">Description</h1>
           <p>
             Lorem ipsum dolor, sit amet consectetur adipisicing elit.
@@ -33,27 +45,39 @@ const CourseDetail = () => {
             inventore. Vero quisquam inventore mollitia?
           </p>
           <div className="my-5">
-            <Card className="p-3">
-              <CardHeader> 
-              <CardTitle className="font-bold text-xl">Course Content</CardTitle>
-              <CardDescription className="px-1">no of lectures</CardDescription>
+            <Card className="md:w-1/2 w-full md:mx-auto ml-3">
+              <CardHeader>
+                <CardTitle className="font-bold text-xl">
+                  Course Content
+                </CardTitle>
+                <CardDescription className="px-1">
+                  no of lectures
+                </CardDescription>
               </CardHeader>
               <CardContent className="space-y-3">
-            {
-                [1,2,3].map((lecture, idx) => (
-                  <div key={idx}>
+                {[1, 2, 3].map((lecture, idx) => (
+                  <div key={idx} className="flex items-center gap-3 text-sm">
                     <span>
-                        {
-                            true ? (<PlayCircle size={14}/>) : <Lock size={14}/>
-                        }
-                    </span>
+                      {true ? <PlayCircle size={14} /> : <Lock size={14} />}
+                    </span>{" "}
+                    <p>Lecture title</p>
                   </div>
-                )
-                )
-            }
+                ))}
               </CardContent>
             </Card>
           </div>
+        </div>
+        <div className="w-full lg:1/3 ">
+                <Card className="md:w-3/4 md:mx-35 ml-5 mb-5">
+                    <CardContent className="p-4 flex flex-col">
+                        <div className="w-full aspect-video mb-4">
+                            Video
+                        </div>
+                        <h1>Lecture title</h1>
+                        <Separator className="my-2"/>
+                        <h1 className=""></h1>
+                    </CardContent>
+                </Card>
         </div>
       </div>
     </div>
