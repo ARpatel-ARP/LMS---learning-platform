d8bab10011747cb24e7f6ff66b653bd15e937d0a
ARpatel-ARP <rautanky407@gmail.com>
2026-06-10 18:32:41 +0530

    changed title

--- frontend/src/Pages/student/Courses.jsx
+++ frontend/src/Pages/student/Courses.jsx
@@ -10,7 +10,7 @@ const Courses = () => {
     <div className="bg-amber-200/20 dark:bg-black">
       <div className="max-w-7xl mx-auto p-4">
         <h2 className="font-bold text-4xl text-center mt-6 mb-10 ">
-          Our Courses
+          Our Published Courses
         </h2>
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
           {isLoading
